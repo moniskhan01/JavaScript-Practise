d5bc68f65d59710a57970c49f9d45f6649fa43d7
moniskhan01 <37439365+moniskhan01@users.noreply.github.com>
2024-01-04 17:39:55 +0000

    javascripts some basics part 1

---
+++ 01_basic_JS/01_basic_js.js
@@ -0,0 +1,360 @@
+// console.log('MONIS KHAN')
+// const id= Symbol('123')
+// const user=Symbol('123')
+
+// console.log(id===user);
+// console.log(id,user);
+
+// const myDate = new Date(2023,11,30)
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toDateString());
+
+// const myArr=[0,1,2,3,4,5]
+
+// console.log('Before Slice',myArr);
+
+// console.log(myArr.slice(1,4));
+// console.log('After Slice',myArr);
+
+// console.log(myArr.splice(1,4));
+// console.log('After Splice',myArr);
+
+
+// const accountId = 144553
+// let accountEmail = "monis@google.com"
+// var accountPassword = "12345"
+// accountCity = "Deoband"
+// let accountState;
+
+// accountId = 2 // not allowed
+
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Noida"
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
+
+// Data Types
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Monis")
+
+
+// let name = "monis"
+// let age = 18
+// let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+
+
+
+
+
+
+
+
+// Conversion Operator
+
+// let score = "Monis"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = "Monis"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Monis"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);11
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+// comparison 
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);2
+
+// === 
+
+// console.log("2" === 2);
+
+
+
+
+
+
+
+// string 
+
+// const name1 = "Monis "
+// const repoCount = 50
+
+// console.log(name1 + repoCount + " Value");
+
+// console.log(`Hello my name is ${name1} and my repo count is ${repoCount}`);
+
+// const gameName = new String('monis-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   Monis    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+
+
+
+// nums and math
+ 
+// const score1 = 400
+// console.log(score1);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+// date and time
+
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+    
+// })
+
+
+
+
+//data types summary
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
