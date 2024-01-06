8f5831581f4df5c689854d5f18aaa4ee0495c260
moniskhan01 <37439365+moniskhan01@users.noreply.github.com>
2024-01-06 20:56:50 +0000

    some basic changes

--- 01_basic_JS/01_basic_js.js
+++ 01_basic_JS/01_basic_js.js
@@ -67,7 +67,7 @@ because of issue in block scope and functional scope
 // let name = "monis"
 // let age = 18
 // let isLoggedIn = false
-// let state;
+let state;
 
 // number => 2 to power 53
 // bigint
